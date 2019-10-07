36fb96b219570a9df3bee4628e1b3363ded6e0e4
Alex Wright <jawright94@gmail.com>
2019-10-07 09:37:14 -0400

    initial commit

---
+++ build/precache-manifest.4506b75a65050862152457eb5b304062.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac8caf401d616fed177742e70961b1c",
+    "url": "/daily-cultures-2019/index.html"
+  },
+  {
+    "revision": "beac5591347fc8e31844",
+    "url": "/daily-cultures-2019/static/css/main.e20da674.chunk.css"
+  },
+  {
+    "revision": "169694922d4899e0c801",
+    "url": "/daily-cultures-2019/static/js/2.0e3ce278.chunk.js"
+  },
+  {
+    "revision": "beac5591347fc8e31844",
+    "url": "/daily-cultures-2019/static/js/main.06ecae64.chunk.js"
+  },
+  {
+    "revision": "b29d069286467ea62112",
+    "url": "/daily-cultures-2019/static/js/runtime-main.df18cbb6.js"
+  },
+  {
+    "revision": "966a16f2b659e94aa8be509055834637",
+    "url": "/daily-cultures-2019/static/media/cameo.966a16f2.png"
+  },
+  {
+    "revision": "9872443aa642d5a2627b162474c58a1b",
+    "url": "/daily-cultures-2019/static/media/cameo2.9872443a.png"
+  },
+  {
+    "revision": "1c73a85435ea02bbbf47cbb3871bda12",
+    "url": "/daily-cultures-2019/static/media/cameo3.1c73a854.png"
+  },
+  {
+    "revision": "23235363cb21c2b16fccd6f8be9b8fa3",
+    "url": "/daily-cultures-2019/static/media/digestive.23235363.png"
+  },
+  {
+    "revision": "638b125a4f759253faffd16475d05d7f",
+    "url": "/daily-cultures-2019/static/media/gut.638b125a.png"
+  },
+  {
+    "revision": "af07467481391bb079d3aeb50da8ca97",
+    "url": "/daily-cultures-2019/static/media/gutHappyText.af074674.png"
+  },
+  {
+    "revision": "1b4ea786881f241572b3b252c595fa48",
+    "url": "/daily-cultures-2019/static/media/intro-img.1b4ea786.png"
+  },
+  {
+    "revision": "b494c7914d19a86215e5cc5dcf5d2939",
+    "url": "/daily-cultures-2019/static/media/left-half-1.b494c791.png"
+  },
+  {
+    "revision": "804799b68ddb7ab18eb3be14a928943b",
+    "url": "/daily-cultures-2019/static/media/left.804799b6.png"
+  },
+  {
+    "revision": "1f4f386a2897741a7a29cfcc64d1eabc",
+    "url": "/daily-cultures-2019/static/media/logo-v1.1f4f386a.png"
+  },
+  {
+    "revision": "7b40e7d4b6a79cc5b8e4647ebba1fd9b",
+    "url": "/daily-cultures-2019/static/media/logo.7b40e7d4.png"
+  },
+  {
+    "revision": "dc18d28271f541244d423ce2157dd139",
+    "url": "/daily-cultures-2019/static/media/logo.dc18d282.png"
+  },
+  {
+    "revision": "05f96b1c9ef11c61e5daa3fe3ba33f80",
+    "url": "/daily-cultures-2019/static/media/mugGrin.05f96b1c.png"
+  },
+  {
+    "revision": "5cbe41d6826f82aa0a0305b143cd89be",
+    "url": "/daily-cultures-2019/static/media/pack.5cbe41d6.png"
+  },
+  {
+    "revision": "231fa3b50f3151326cf1abfa2f7f00e8",
+    "url": "/daily-cultures-2019/static/media/pack2.231fa3b5.png"
+  },
+  {
+    "revision": "8239eec2ab90a4905d8d954d65f888cc",
+    "url": "/daily-cultures-2019/static/media/packs.8239eec2.png"
+  },
+  {
+    "revision": "c8d86ad71eed49c40467a485884a31c1",
+    "url": "/daily-cultures-2019/static/media/right.c8d86ad7.png"
+  },
+  {
+    "revision": "5ed18ee5a7b444500cfc085cd250b6a8",
+    "url": "/daily-cultures-2019/static/media/text.5ed18ee5.png"
+  },
+  {
+    "revision": "adf51743bedf62640c80bd518276427f",
+    "url": "/daily-cultures-2019/static/media/text.adf51743.png"
+  },
+  {
+    "revision": "5c18d68c6b81190490a280cb870a1c8e",
+    "url": "/daily-cultures-2019/static/media/twinkles.5c18d68c.png"
+  },
+  {
+    "revision": "a9737cf2edf1141d5d58720e58c8cf35",
+    "url": "/daily-cultures-2019/static/media/twinkles.a9737cf2.png"
+  }
+]);
